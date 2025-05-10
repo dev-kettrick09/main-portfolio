c7808f5368f690fd073819731641acaf1b8d01d6
John Kettrick Tejones <dev.johnkettricktejones@gmail.com>
2025-05-10 23:52:33 +0800

    051025

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,23 @@ if(navClose){
   })
 }
 
+// Smooth scrolling for navigation links
+const navLinks = document.querySelectorAll('.nav__link');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent default anchor behavior
+    const targetId = this.getAttribute('href').substring(1); // Get the target secti on ID
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: 'smooth', // Smooth scrolling
+        block: 'start', // Align to the top of the section
+      });
+    }
+  });
+});
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
@@ -98,24 +115,38 @@ modalCloses.forEach((modalClose) =>{
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiper = new Swiper('.portfolio__container',{
-  cssMode: true,
+  loop: true,
+  effect: 'slide',
+  speed: 600,
   navigation: {
-    nextE1: '.swiper-button-next',
-    prevE1: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   pagination: {
-    el: '.swiper-pagination'
+    el: '.swiper-pagination',
+    clickable: true,
   },
-  mousewheel: true,
-  keyboard: true,
 });
 
-
-/*==================== TESTIMONIAL ====================*/
-
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 
